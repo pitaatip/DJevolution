0b6a5296c8dd7f12d5858f4d8695f6f1f58c82c9
Piotr Lesniak <pita_atip@tlen.pl>
2012-04-10 20:43:33 +0200

    Folders changed

---
+++ deap-examples/examples/benchmarks/test_all.py
@@ -0,0 +1,25 @@
+'''
+Created on 10-04-2012
+
+@author: pita
+'''
+import sys
+from deap import benchmarks, tools
+
+def rastrigin_arg0(sol):
+    return benchmarks.rastrigin(sol)[0]
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3 or len(sys.argv) %2 != 1:
+        print "Use python test_all.py name_of_module1 name_of_func1 name_of_module2 name_of_func2"
+        exit()
+    for i in xrange(0,len(sys.argv)-2,2):
+        module = __import__(sys.argv[i+1])
+        func = getattr(module,sys.argv[i+2])
+        print "-----"
+        print "Executing %s.%s()..."%(sys.argv[i+1],sys.argv[i+2])
+        list_of_ind = [func() for _ in xrange(10)]
+        list_of_val = map(rastrigin_arg0,list_of_ind)
+        print "\nAVG: ",tools.mean(list_of_val)
+        print "BEST: ",min(list_of_val)
+        
