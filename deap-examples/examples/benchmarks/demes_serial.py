71fb6d19d18e1ce05d795128511d77b0db640b08
Wysek <wysek@student.agh.edu.pl>
2012-04-21 18:45:18 +0200

    dtm/serial island model corrected(?) and true island model using PIPES/MPI added

--- deap-examples/examples/benchmarks/demes_serial.py
+++ deap-examples/examples/benchmarks/demes_serial.py
@@ -10,13 +10,16 @@ import sys
 import logging
 import random
 import algorithms_helper
+import array
+from math import cos, pi
+
 
 _logger = logging.getLogger("deap.algorithms")
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
 
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-creator.create("Individual", numpy.ndarray, fitness=creator.FitnessMin) #@UndefinedVariable
+creator.create("Individual", array.array, typecode='f', fitness=creator.FitnessMin) #@UndefinedVariable
 
 toolbox = base.Toolbox()
 toolbox.register("attr", lambda :numpy.random.random() * 10 - 5)
@@ -35,9 +38,9 @@ toolbox.register("select", tools.selTournament, tournsize=7)
 
 MU = 140
 LAMBDA = 280
-N_GEN = 220
-DEMES_NR = 5
-MIG_RATE = 20
+N_GEN = 1000
+DEMES_NR = 3
+MIG_RATE = 5
 MIG_RATIO = 0.1
 
 toolbox.register("migrate", tools.migRing, k=int(MIG_RATIO * MU), selection=tools.selBest,
@@ -45,55 +48,25 @@ toolbox.register("migrate", tools.migRing, k=int(MIG_RATIO * MU), selection=tool
 
 def evalDeme(deme):
     deme[:] = [toolbox.clone(ind) for ind in toolbox.select(deme, len(deme))]
-    algorithms_helper.varLambda(toolbox, deme, LAMBDA, 0.3, 0.5)
+#    algorithms_helper.varLambda(toolbox, deme, LAMBDA, 0.5, 0.3)
+    algorithms.varOr(deme, toolbox, LAMBDA, 0.5, 0.3)
     
     for ind in deme:
         ind.fitness.values = toolbox.evaluate(ind)
     
     return deme
-                 
-#computes eaMuPlusLambda for N_GEN generation
-def compute():
-    random.seed(47)
-    pop = toolbox.population(n=MU)
-    hof = tools.HallOfFame(1)
-        
-    algorithms.eaMuPlusLambda(toolbox, pop, MU, LAMBDA, 0.3 , 0.5, N_GEN, hof)
-    return sorted(list(hof[-1]))
-    
-    #return pop, stats, hof
 
 def final(x):
     a = benchmarks.rastrigin(x)
     print "rastrigin: ",a
     return (a > 1)
-    
-#computes eaMuPlusLambda until func is smaller than 1    
-def compute_till_best():
-    random.seed(47)
-#    pop = toolbox.population(n=MU)
-    #demes = [toolbox.population(n=MU) for _ in xrange(NBR_DEMES)]
-    hof = tools.HallOfFame(1)
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("Avg", tools.mean)
-    stats.register("Std", tools.std)
-    stats.register("Min", min)
-    stats.register("Max", max)
-    
-    
-    #algorithms_helper.eaMuPlusLambda(toolbox, pop, 100, 100, 0.7 , 0.01, N_GEN, halloffame=hof, stats=stats,final_stop=final)
-    
-    return sorted(list(hof[-1]))
-    
-    #return pop, stats, hof
-    
+
 #computes eaMuPlusLambda until func is smaller than 1    
 def compute_till_best2():
     random.seed(47)
-    pop = toolbox.population(n=MU)
     demes = [toolbox.population(n=MU) for _ in xrange(DEMES_NR)]
     hof = tools.HallOfFame(1)
-    stats = tools.Statistics(lambda ind: ind.fitness.values, DEMES_NR)
+    stats = tools.Statistics(lambda ind: ind.fitness.values, DEMES_NR+1)
     stats.register("Avg", tools.mean)
     stats.register("Std", tools.std)
     stats.register("Min", min)
@@ -108,15 +81,16 @@ def compute_till_best2():
         hof.update(deme)
         logger.logGeneration(gen="0.%d" % idx, evals=len(deme), stats=stats, index=idx)
     
-    stats.update(demes[0]+demes[1]+demes[2], 3)
-    logger.logGeneration(gen=0, evals="-", stats=stats, index=3)
+    stats.update(demes[0]+demes[1]+demes[2], DEMES_NR)
+    logger.logGeneration(gen=0, evals="-", stats=stats, index=DEMES_NR)
     
     gen = 1
     while gen <= N_GEN:
+    #not len([final(dem) for dem in demes]) > 0 :
         
         # We map the evaluation loop
         demes[:] = map(evalDeme, demes)
-        #emes[:] = dtm.map(evalDeme, demes)
+#        demes[:] = dtm.map(evalDeme, demes)
         for idx, deme in enumerate(demes):
             # To remain a simple example, we do not parallelize stats evaluation (which would have
             # been possible, but imply some subtleties)
@@ -126,21 +100,23 @@ def compute_till_best2():
             
         if gen % MIG_RATE == 0:
             toolbox.migrate(demes)
-        stats.update(demes[0]+demes[1]+demes[2], 3)
-        logger.logGeneration(gen="%d" % gen, evals="-", stats=stats, index=3)
+        stats.update(demes[0]+demes[1]+demes[2], DEMES_NR)
+#        logger.logGeneration(gen="%d" % gen, evals="-", stats=stats, index=3)
         gen += 1
     
-        
-    #algorithms_helper.eaMuPlusLambda(toolbox, pop, 100, 100, 0.4 , 0.3, N_GEN, stats=stats, halloffame=hof, final_stop=final)
-    
-    return sorted(list(hof[-1]))
+    return hof[0]
+#    sorted(list(hof[-1]))
+#    return demes, stats, sorted(list(hof[-1]))
     
     #return pop, stats, hof
 
 def main():
     best_in=compute_till_best2()
-    logging.critical("Best individual is %s, %s", (lambda x: sorted(list(x)))(best_in)) 
-    
+#    print best_in
+#    logging.critical("Best individual is %s, %s", (lambda x: sorted(list(x)))(best_in))
+    logging.critical("Best individual is %s with value %s" % (best_in, best_in.fitness.values))
+#    rastrigin(best_in)
 
 if __name__ == '__main__':
     main()
+#    dtm.start(main)
