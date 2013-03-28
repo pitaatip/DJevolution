241a82b0f8c4b4c117d32f0bccfab3d9f680889b
Piotr Lesniak <pita_atip@tlen.pl>
2013-03-28 22:27:08 +0100

    One more benchmark 3-dim problem

--- VisualController/VisualControllerApp/problems.py
+++ VisualController/VisualControllerApp/problems.py
@@ -1,3 +1,5 @@
+from math import cos, pi, sqrt
+
 __author__ = 'pita'
 
 from deap import benchmarks
@@ -8,3 +10,11 @@ for foo_k in benchmarks.__dict__:
 
 def three_obj(individual):
     return individual[0], individual[0] ** 2, (individual[0] - 2) ** 2
+
+def uber_three(individual):
+    g  = 1 + 10*(len(individual)-1) + sum(xi**2 - 10*cos(4*pi*xi) for xi in individual[1:])
+    g2  = 1.0 + 9.0*sum(individual[1:])/(len(individual)-1)
+    f1 = individual[0]
+    f2 = g * (1 - sqrt(f1/g))
+    f3 = g2 * (1 - sqrt(f1/g2))
+    return f1,f2,f3
